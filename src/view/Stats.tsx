4560f89805fc7a7adf5259c2975d165272f89c05
Miligen <angelo0204@hotmail.fr>
2023-11-05 19:29:34 +0100

    Avancement stats

--- src/view/Stats.tsx
+++ src/view/Stats.tsx
@@ -25,23 +25,23 @@ function Stats(){
     }
     return(
 
-      <>
+    
       <div className="container">
         <div className="leftCells">
 
-          <div className="Cell">
+          <div className="Cell bgCell">
             <div className="titleCell">
               <h6>Ranked Solo</h6>
             </div>
             <div className="infoCell">
 
               <div className="iconeCell">
-                <img src="/Ranked-Emblems-Latest/Rank=Emerald.png" width={150} alt="" />
+                <img src="/Ranked-Emblems-Latest/Rank=Emerald.png" width={80} alt="" />
               </div>
 
               <div className="rankCell">
                 <h4>Emerald</h4>
-                <h5>20LP</h5>
+                <h6>20LP</h6>
               </div>
 
               <div className="winrate">
@@ -52,7 +52,7 @@ function Stats(){
 
           </div>
 
-          <div className="Cell">
+          <div className="Cell bgCell">
 
             <div className="titleCell">
               <h6>Ranked Flex</h6>
@@ -61,12 +61,12 @@ function Stats(){
             <div className="infoCell">
 
               <div className="iconeCell">
-                <img src="/Ranked-Emblems-Latest/Rank=Diamond.png" width={150} alt="" />
+                <img src="/Ranked-Emblems-Latest/Rank=Diamond.png" width={80} alt="" />
               </div>
 
               <div className="rankCell">
                 <h4>Diamond</h4>
-                <h5>20LP</h5>
+                <h6>20LP</h6>
               </div>
 
               <div className="winrate">
@@ -77,12 +77,75 @@ function Stats(){
             </div>
 
           </div>
+
+          <div className="Cell bgCell">
+            <div className="titleCell">
+              <h5>Carriere</h5>
+            </div>
+            <div className="infosCellCarriere">
+              <div className="firstLigne">
+                <div className="timeGame">
+                  <h2>1D 16H 22M</h2>
+                  <h5>Temps de jeu</h5>
+
+                </div>
+                <div className="gamePlayed">
+                  <h2>279</h2>
+                  <h5>partie jouées</h5>
+                </div>
+              </div>
+              <div className="secondLigne">
+                <div className="donut">
+                  <div className="donutHole">
+                    <h2>50%</h2>
+                  </div>
+
+                </div>
+
+              </div>
+            </div>
+
+
+          </div>
+
           
         </div>
+
+        <div className="rightCells">
+          <div className="cell2">
+            <div className="type">
+              <div className="flex"><h5>Flex 5C5 Rank</h5></div>
+              <div className="quand"><p>il y a un jour</p></div>
+              <hr width={50}></hr>
+              <div className="gameEtat"><p>Defaite</p></div>
+              <div className="timePlayed"><p>29:50</p></div>
+            </div>
+            <div className="iconeChamp&summ">
+              <div className="imgChamp">
+                <img src="/LoL_Icon_Rendered_Hi-Res.png" width={80} />
+              </div>
+              <div className="item">
+                
+                
+              </div>
+
+            </div>
+            <div className="summoners">
+
+            </div>
+
+
+
+
+          </div>
+
+        </div>
+
+
       </div>
       
       
-      </>
+
         
 
         
